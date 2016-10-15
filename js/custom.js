fc0c4b58b159a53ca99dbf71053512a445ba28b3
Giang <giang2b@gmail.com>
2016-10-15 20:11:25 +0700

    modify

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+	$(document).on('click', '.close-nav', function(){
+		$('button.navbar-toggle').trigger('click');
+	});
 
+//	$(document).on('click', '#navbar li:not(.close-nav):not(.active)', function(){
+//		// $('#navbar li.active').find('span').removeClass('navbar-box').addClass('navbar-underscore');
+//		$('#navbar li.active').removeClass('active');
+//		$(this).addClass('active');
+//	//	$(this).find('span').addClass('navbar-box').removeClass('navbar-underscore');
+//	});
 });
