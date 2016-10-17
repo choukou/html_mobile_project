2e7109a0e616bfa9ae0955451e618fbdc6954966
Giang <giang2b@gmail.com>
2016-10-18 00:33:43 +0700

    @

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
+	$(document).on('click', '.close-nav', function(){
+		$('button.navbar-toggle').trigger('click');
+	});
 
+	$(document).on('click', '#navbar li:not(.close-nav):not(.active)', function(){
+		// $('#navbar li.active').find('span').removeClass('navbar-box').addClass('navbar-underscore');
+		$('#navbar li.active').removeClass('active');
+		$(this).addClass('active');
+	//	$(this).find('span').addClass('navbar-box').removeClass('navbar-underscore');
+	});
 });
+
+
+
+$(document).ready(function() {
+ 
+  var owl = $("#three-biz");
+ 
+  owl.owlCarousel({
+	  autoPlay: 2000,
+	  loop: false,
+	  center: true,
+	  rewind: true,
+	  nav:false,
+	  dots: false,
+	  dotsEach: false,
+	  dotData: false,
+	  pagination:false,
+      itemsCustom:[[0,1],[450,1],[700,2],[1000,3]]
+ 
+  });
+ 
+});
+
