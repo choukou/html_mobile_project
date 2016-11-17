e25844c80108eac324b47fe813dc66ebff9b5dcc
Giang <giang2b@gmail.com>
2016-11-17 08:11:50 +0700

    a

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-
+	$(window).on('resize', function() {
+//		fixHeightByImg('#content6, #content7');
+	});
+//	fixHeightByImg('#content6, #content7');
 });
+
+//extend for jquery
+//$.fn.extend({
+//	showToolTipHand: function(selector){
+//
+//	}
+//});
+
+function fixHeightByImg(selector) {
+	$(selector).each(function(){
+		var img = $(this).find('img');
+		$(this).height(img.height());
+	});
+
+}
